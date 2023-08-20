5227c4f8f7ef7d127d2151bee30a94342a50f69f
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-08-21 01:02:54 +0430

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,88 @@
+// swiper
+
+var menuSwiper = new Swiper(".banner", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+var menuSwiper = new Swiper(".machines", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+    1200: {
+      slidesPerView: 4
+    },
+    992: {
+      slidesPerView: 3
+    },
+    576: {
+      slidesPerView: 2
+    },
+  }
+});
+var menuSwiper = new Swiper(".brands", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+    1200: {
+      slidesPerView: 6
+    },
+    992: {
+      slidesPerView: 4
+    },
+    576: {
+      slidesPerView: 2
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+// fancybox
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+// animation
+
+ScrollReveal().reveal('.top', {
+  duration: 2000,
+  origin: 'top',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.bottom', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '100px',
+  delay: 500,
+  easing: 'ease-out'
+});
