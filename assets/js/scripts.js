0ee8e924443d11cef17dc094d4a090d5a2ec182f
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-19 14:34:42 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,30 @@
 
 // mobile menu
 
-let hamIcon=document.getElementById('hamIcon');
+let hamIcon = document.getElementById('hamIcon');
 
-hamIcon.addEventListener('click',function () {
+hamIcon.addEventListener('click', function () {
   hamIcon.nextElementSibling.querySelector('nav').classList.add('active');
   document.getElementById('overlay').classList.add('active');
-  document.getElementById('overlay').addEventListener('click',function () {
+  document.getElementById('overlay').addEventListener('click', function () {
     this.classList.remove('active');
     this.nextElementSibling.querySelector('nav').classList.remove('active');
   })
 })
+
 // filter
 
-let filterBtn=Array.from(document.getElementsByClassName('filterBtn'));
-let filterContent=Array.from(document.getElementsByClassName('filterContent'));
+let filterBtn = Array.from(document.getElementsByClassName('filterBtn'));
+let filterContent = Array.from(document.getElementsByClassName('filterContent'));
 
-filterBtn.forEach((item)=>{
-  item.addEventListener('click',function () {
-    item.nextElementSibling.classList.toggle('active');
-    item.querySelector('svg').classList.toggle('active');
+filterBtn.forEach((item) => {
+  item.addEventListener('click', function () {
+    filterBtn.forEach((items) => {
+      items.nextElementSibling.classList.remove('active');
+      items.querySelector('svg').classList.remove('active');
+    })
+    item.nextElementSibling.classList.add('active');
+    item.querySelector('svg').classList.add('active');
   })
 })
 
@@ -71,6 +76,22 @@ var menuSwiper = new Swiper(".brands", {
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
+  },
+});
+var smallImgs = new Swiper(".smallImgs", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var gallery = new Swiper(".gallery", {
+  spaceBetween: 10,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+      Swiper
   },
 });
 // fancybox
