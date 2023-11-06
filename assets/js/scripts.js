23e3088f397d9340dbf8b55a4d9a1ac55bd8a1c2
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-11-06 22:04:23 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -34,6 +34,7 @@ smallImgs.forEach((tab) => {
 
 let filterBtn = Array.from(document.getElementsByClassName('filterBtn'));
 let filterContent = Array.from(document.getElementsByClassName('filterContent'));
+let checkAllInput = Array.from(document.getElementsByClassName('checkAll'));
 
 filterBtn.forEach((item) => {
   item.addEventListener('click', function () {
@@ -46,6 +47,32 @@ filterBtn.forEach((item) => {
   })
 })
 
+checkAllInput.forEach((item)=>{
+  item.addEventListener('click',function () {
+    item.classList.toggle('active');
+  })
+})
+
+function check(checked = true) {
+  const checkboxes = document.querySelectorAll('input[name="type"]');
+  checkboxes.forEach((checkbox) => {
+      checkbox.checked = checked;
+  });
+}
+
+
+const btn = document.querySelector('#btn');
+btn.onclick = checkAll;
+
+function checkAll() {
+  check();
+  this.onclick = uncheckAll;
+}
+
+function uncheckAll() {
+  check(false);
+  this.onclick = checkAll;
+}
 // swiper
 
 var menuSwiper = new Swiper(".banner", {
