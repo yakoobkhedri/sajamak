61a57e8a07ec408305b3d20f68d99f8643468c3b
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-11-08 22:00:51 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,49 +30,6 @@ smallImgs.forEach((tab) => {
   })
 })
 
-// filter
-
-let filterBtn = Array.from(document.getElementsByClassName('filterBtn'));
-let filterContent = Array.from(document.getElementsByClassName('filterContent'));
-let checkAllInput = Array.from(document.getElementsByClassName('checkAll'));
-
-filterBtn.forEach((item) => {
-  item.addEventListener('click', function () {
-    filterBtn.forEach((items) => {
-      items.nextElementSibling.classList.remove('active');
-      items.querySelector('svg').classList.remove('active');
-    })
-    item.nextElementSibling.classList.add('active');
-    item.querySelector('svg').classList.add('active');
-  })
-})
-
-checkAllInput.forEach((item)=>{
-  item.addEventListener('click',function () {
-    item.classList.toggle('active');
-  })
-})
-
-function check(checked = true) {
-  const checkboxes = document.querySelectorAll('input[name="type"]');
-  checkboxes.forEach((checkbox) => {
-      checkbox.checked = checked;
-  });
-}
-
-
-const btn = document.querySelector('#btn');
-btn.onclick = checkAll;
-
-function checkAll() {
-  check();
-  this.onclick = uncheckAll;
-}
-
-function uncheckAll() {
-  check(false);
-  this.onclick = checkAll;
-}
 // swiper
 
 var menuSwiper = new Swiper(".banner", {
@@ -123,6 +80,51 @@ var menuSwiper = new Swiper(".brands", {
     dynamicBullets: true,
   },
 });
+
+// filter
+
+let filterBtn = Array.from(document.getElementsByClassName('filterBtn'));
+let filterContent = Array.from(document.getElementsByClassName('filterContent'));
+let checkAllInput = Array.from(document.getElementsByClassName('checkAll'));
+
+filterBtn.forEach((item) => {
+  item.addEventListener('click', function () {
+    filterBtn.forEach((items) => {
+      items.nextElementSibling.classList.remove('active');
+      items.querySelector('svg').classList.remove('active');
+    })
+    item.nextElementSibling.classList.add('active');
+    item.querySelector('svg').classList.add('active');
+  })
+})
+
+checkAllInput.forEach((item)=>{
+  item.addEventListener('click',function () {
+    item.classList.toggle('active');
+  })
+})
+
+function check(checked = true) {
+  const checkboxes = document.querySelectorAll('input[name="type"]');
+  checkboxes.forEach((checkbox) => {
+      checkbox.checked = checked;
+  });
+}
+
+
+const btn = document.querySelector('#btn');
+btn.onclick = checkAll;
+
+function checkAll() {
+  check();
+  this.onclick = uncheckAll;
+}
+
+function uncheckAll() {
+  check(false);
+  this.onclick = checkAll;
+}
+
 // fancybox
 
 Fancybox.bind("[data-fancybox]", {
